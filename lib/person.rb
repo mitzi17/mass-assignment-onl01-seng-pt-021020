b8ccca78d5dce80d0c2d326004bb69bc53bb9f7a
Mitzi Estrada <m_estrada17@live.com>
2020-03-14 04:56:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initizlize(attributes=nil)
+  def initizlize
    attributes.each {|key, value| self.send(("#{key}="), value)}
 
 end
